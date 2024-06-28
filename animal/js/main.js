f8a122e7f279d2023d5562735f1e580774764b56
Tencarrothill <ultrapandemonium@gmail.com>
2024-06-28 18:00:35 +0900

    0628 animal

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,73 +1,60 @@
 $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
         autoplay: {  /* 팝업 자동 실행 */
-            delay: 3000,
+            delay: 3500,
             disableOnInteraction: true,
         },
-        effect: "fade", /* fade 효과 */
+
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
         navigation: {  /* 이전, 다음 버튼 */
-            nextEl: '.visual .btn_prev',  /* 다음 버튼의 클래스명 */
-            prevEl: '.visual .btn_next',  
+            nextEl: '.visual .btn_next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .btn_prev',  
         },
+
     });//visual_swiper
-
-    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
-        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
-        breakpoints: {
-            640: {  /* 640px 이상이 되면 적용 */
-            slidesPerView: "auto",
-                spaceBetween: 24,
-            },
-        },
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    });//book_swiper
-
     /*
-        스크롤을 내리면 header에 fixed 클래스를 추가할 것임
-        스크롤이 맨 위에 있을 땐 fixed 클래스 삭제. 조금이라도 내려오면 fixed 클래스 부여.
-        브라우저가 스크롤 된 정도를 체크하는 값 : $(window).scrollTop()
-        맨 위 0 , 스크롤 내리면 상단부터 스크롤 한 만큼 출력.
-        $(window).scrollTop()가 0이면 fixed을 삭제, 0보다 크면 fixed 부여.
-     */
-    $(window).scroll(function(){ //browser가 스크롤 될 때 마다 실행되는 명령.
-        $(window).scrollTop()
-        console.log($(window).scrollTop())
-        if($(window).scrollTop() > 0){
-            $('header').addClass('fixed')
-            /* header에 class명fixed을 준다는 말 */
-        }else{ //그 외 전부
-            $('header').removeClass('fixed')
-            /* header에 줬던 class명 fixed을 지운다는 말 */
-        }//if
-    })//scroll
+        일시정지 버튼 클릭시,
+        팝업은 일시정지, 일시정지 버튼 숨김처리
+        재생 버튼 나타남
 
-    /*
-    .interview .list ul li
-    오버한 li에만 class:'on'을 주고 그 외 li와 이전에 오버상태였던 li는 class:'on' 삭제.
-    --> 이전에 오버했던 li을 선택하는 명령이 없다. 때문에, 모든 li의 class:'on'을 다 삭제 후
-    오버한 li에만 다시 class:'on' 부여.
-    <ul>
-        <li></li> -- when over, 이 li만 지칭하는 언어 $(this)
-        <li></li>
-        <li></li>
-    </ul>
+        재생 버튼 클릭시,
+        팝업 재생됨
+        재생 버튼 숨김처리
+        일시정지 버튼 나타남
     */
-   $('.interview .list ul li').on('mouseenter', function(){
-    $('.interview .list ul li').removeClass('on')
-    $(this).addClass('on')
-   })
+   $('.visual .btn_wrap button.btn_pause').on('click', function(){
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide()
+        $('.visual .btn_wrap button.btn_play').show()
+   })//btn_pause
+   $('.visual .btn_wrap button.btn_play').on('click', function(){
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide()
+        $('.visual .btn_wrap button.btn_pause').show()
+   })//btn_play
 
-   const series_swiper = new Swiper('.series .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        breakpoints: {
-            640: {    /* 640px 이상일때 적용 */
-                slidesPerView: 4, //pc ver.
-                spaceBetween: 24,
-            },
-        },
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    });//series swiper
-})//document.ready
+   /*  page가 scroll되면 header에 class=fixed를 추가
+        다시 맨 위로 올라가면 header에 class=fixed를 삭제  */
+    let scrolling
+
+    function scroll_chk(){
+        scrolling = (Window).scrollTop()
+        console.log(scrolling)
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+    }//function
+
+    //문서가 처음 로딩되었을 때 1번 실행
+    scroll_chk()
+
+    $(Window).scroll(function(){
+        //browser을 scroll할 때 마다 실행
+        scroll_chk()
+    })//window.scroll
+
+})//document .ready
