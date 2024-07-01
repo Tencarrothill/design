4819e06e876c660af4ad7fc302e2826f8df1be29
Tencarrothill <ultrapandemonium@gmail.com>
2024-07-01 14:11:30 +0900

    0701 animal

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,4 +1,27 @@
 $(document).ready(function(){
+
+    /* browser 너비가 1024이하면 모바일, 이상이면 pc. */
+    let pc_mobile // 현재 상태가 pc인지 mobile인지를 저장
+    let window_w // browser 너비 저장
+
+    function resize_chk(){
+        window_w = $(window).width()
+        if(window_w > 1024){ /* 1024보다 크면 - pc면 */
+            pc_mobile = 'pc'
+        }else{ /* mobile이면 */
+            pc_mobile = 'mobile'
+        }
+        console.log(pc_mobile)
+    }
+    // 처음 로딩 됐을 때 실행
+    resize_chk()
+    // 브라우저가 리사이즈 될 때 마다 실행
+    $(window).resize(function(){
+        resize_chk()
+    })
+
+
+
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -40,7 +63,7 @@ $(document).ready(function(){
     let scrolling
 
     function scroll_chk(){
-        scrolling = (Window).scrollTop()
+        scrolling = $(window).scrollTop()
         console.log(scrolling)
         if(scrolling > 0){
             $('header').addClass('fixed')
@@ -52,9 +75,44 @@ $(document).ready(function(){
     //문서가 처음 로딩되었을 때 1번 실행
     scroll_chk()
 
-    $(Window).scroll(function(){
+    $(window).scroll(function(){
         //browser을 scroll할 때 마다 실행
         scroll_chk()
     })//window.scroll
+
+    /* 메뉴(header .gnb .gnb_wrap ul.depth1>li)에 마우스를 올리면,
+    header에 class:menu_over 추가.
+    마우스 오버된 li에만 class:on 추가.
+    
+    -> 다른 li(메뉴)에 마우스 오버하면, 이전에 오버했던 메뉴의 class:on 삭제.
+    -> header에서 마우스아웃 되면 class:menu_over 삭제. (이전에 오버했던 메뉴를 알기 어려움)
+        -> 모든 메뉴의 class:on을 삭제했다가 오버한 내 것만 class:on 추가
+        모든 메뉴에서 class:on 삭제 */
+
+    $('header .gnb .gnb_wrap ul.depth1>li').on('mouseenter', function(){
+        if(pc_mobile == 'pc'){ //pc ver. 일 때만 실행하겠다
+            $('header').addClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1>li').removeClass('on')
+            $(this).addClass('on') /* 마우스 오버한 li 당사자. 나 자신 */
+        }// if
+            
+    })
+    $('header').on('mouseleave', function(){
+        if(pc_mobile == 'pc'){ //pc ver. 일 때만 실행하겠다
+            $('header').removeClass('menu_over')
+            $('header .gnb .gnb_wrap ul.depth1>li').removeClass('on')
+        } // if
+    })
+
+    $('header .gnb .gnb_open').on('click', function(){
+        if(pc_mobile == 'mobile'){ //mobile ver. 일 때만 실행하겠다
+            $('header').addClass('mobile_open')
+        }
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+        if(pc_mobile == 'mobile'){ //mobile ver. 일 때만 실행하겠다
+            $('header').removeClass('mobile_open')
+        }
+    })
 
 })//document .ready
