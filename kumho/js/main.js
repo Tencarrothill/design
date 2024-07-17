56824ec163ef9856d8105cf11ff52ad1780989bf
Tencarrothill <ultrapandemonium@gmail.com>
2024-07-17 14:04:37 +0900

    0717 kumho

--- kumho/js/main.js
+++ kumho/js/main.js
@@ -38,4 +38,26 @@ $('document').ready(function(){
         $(this).hide() //숨김
         $('.visual .btn_wrap button.btn_stop').show() //보임
     })
+
+    /* -- pc ver. --
+        .biz .list ul li에게 마우스오버 시,
+        마우스를 올린 li:class=on 추가
+        마우스 아웃되어있는 나머지 li:class=off 추가 */
+        $('.biz .list ul li').on('mouseenter', function(){
+            // if($(window).widht() > 1024){
+            //     $('.biz .list ul li').removeClass('on')
+            //     $('.biz .list ul li').addClass('off')
+            //     $(this).removeClass('off')
+            //     $(this).addClass('on')
+            // 
+            // }
+            $('.biz .list ul li').removeClass('on')
+            $('.biz .list ul li').addClass('off')
+            $(this).removeClass('off')
+            $(this).addClass('on')
+        })
+        $('.biz .list').on('mouseleave', function(){
+            $('.biz .list ul li').removeClass('on')
+            $('.biz .list ul li').removeClass('off')
+        })
 })//document.ready
