7d85194fee911abe6fb1111c8bc2e53d1f587522
Tencarrothill <ultrapandemonium@gmail.com>
2024-09-01 18:27:20 +0900

    0901 meongle

--- p_meongle/js/main.js
+++ p_meongle/js/main.js
@@ -1,7 +1,7 @@
 $('document').ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', {
 		autoplay: {
-			delay: 40000,
+			delay: 4000,
 			disableOnInteraction: true,
 		},
 		loop: true,
@@ -19,6 +19,24 @@ $('document').ready(function(){
 		visual_swiper.autoplay.start();  /* 일시정지 기능 */
 		$(this).hide()
 		$('.visual .btn_wrap .pause').show()
-	})
+	})//visual_swiper
+
+	const reservation_swiper = new Swiper('.reservation .swiper', {
+		slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+		spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+		breakpoints: {
+			881: {  /* 640px 이상이 되면 적용 */
+				spaceBetween: 34, 
+			},
+		},
+		centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+		scrollbar: {
+            el: ".reservation .swiper-scrollbar",
+            hide: false,
+            draggable: true,
+          }
+		
+	});
 
 })//document
