d05bf4437607e586a77fb9668bf7a3e38cea0a81
Tencarrothill <ultrapandemonium@gmail.com>
2024-09-12 13:17:14 +0900

    0912 kma AM

--- p_kma/js/main.js
+++ p_kma/js/main.js
@@ -28,6 +28,25 @@ $('document').ready(function(){
     });//fulpage
 
     //gsap.from(".visual .tit h2", {duration: 3, text: ""})
+
+    const story_swiper = new Swiper('.story .swiper', {
+        slidesPerView: "auto", /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 0, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            1024: {  /* 1024px 이상이 되면 적용 */
+                spaceBetween: 0,
+                slidesPerView: 3
+            },
+        },
+        centeredSlides: false, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.story .list .paging', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            
+        },
+    });
     
     
 })//document.ready
